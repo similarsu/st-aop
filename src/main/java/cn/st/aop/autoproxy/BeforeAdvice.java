6b2a3873ba0dc1a3c490dfb2c59f307c238a417e
sutong <821192673@qq.com>
2015-05-19 10:52:15 +0800

    beannameautoproxy自动代理示例代码

---
+++ src/main/java/cn/st/aop/autoproxy/BeforeAdvice.java
@@ -0,0 +1,27 @@
+package cn.st.aop.autoproxy;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+/**
+ * <p>
+ * description:前置增强
+ * </p>
+ * 
+ * @author coolearth
+ * @since 2015年5月15日
+ * @version v1.0
+ */
+public class BeforeAdvice implements MethodBeforeAdvice {
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        // TODO Auto-generated method stub
+        // 输出切点
+        System.out.println(target.getClass().getName() + "." + method.getName());
+        String clientName = (String) args[0];
+        System.out.println("你好，" + clientName);
+    }
+
+}
---
+++ src/main/java/cn/st/aop/autoproxy/Seller.java
@@ -0,0 +1,7 @@
+package cn.st.aop.autoproxy;
+
+public class Seller {
+    public void greetTo(String name) {
+        System.out.println("seller greet to " + name);
+    }
+}
